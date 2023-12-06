779533fa592e9f377a1f1682acfd3e1e5b65a871
MOHD ADIL <119355639+mohdadil12345@users.noreply.github.com>
2023-12-06 22:27:03 +0530

    frontend

--- frontend/src/components/Billing.jsx
+++ frontend/src/components/Billing.jsx
@@ -1,24 +1,87 @@
-import React, { useEffect } from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 function Billing() {
+  const [bill, setBill] = useState([]);
+  const [postBill, setPostBill] = useState([]);
+  const [quantity, setQuantity] = useState(1);
+  const [item, setItem] = useState("");
 
-const fetchdata = async() => {
-     let res = await fetch(`http://localhost:8080/items`)
+  const postdata = async (quantity, item) => {
+    try {
+      const response = await axios.post("http://localhost:8080/bills/add", {
+        quantity,
+        item,
+      });
+      console.log("res", response.data);
+    } catch (error) {
+      console.error("Error posting data:", error.message);
+    }
+  };
 
-     let data = await res.json()
-     console.log(data)
-}
+  const formSubmit = () => {
+    postdata(quantity, item);
+  };
 
-useEffect(() => {
-    fetchdata()
-}, [])
+  const fetchData = async () => {
+    try {
+      const response = await fetch("http://localhost:8080/items");
+      const data = await response.json();
+      setBill(data);
+    } catch (error) {
+      console.error("Error fetching data:", error.message);
+    }
+  };
 
+  const fetchBills = async () => {
+    try {
+      const response = await fetch("http://localhost:8080/bills");
+      const data = await response.json();
+      setPostBill(data);
+    } catch (error) {
+      console.error("Error fetching data:", error.message);
+    }
+  };
 
-
+  useEffect(() => {
+    fetchData();
+    fetchBills();
+  }, []);
 
   return (
-    <div>Billing</div>
-  )
+    <div>
+      <select onChange={(e) => setItem(e.target.value)}>
+        {bill.length > 0 &&
+          bill.map((ele) => (
+            <option key={ele._id} value={ele._id}>
+              {ele.name}
+            </option>
+          ))}
+      </select>
+      <br />
+      <input
+        type="text"
+        onChange={(e) => setQuantity(e.target.value)}
+        placeholder="quantity"
+      />
+      <br />
+      <button onClick={formSubmit}>Add</button>
+
+      <div>
+        {postBill.map((e) => (
+          <div key={e._id}>
+            {e.items.map((item) => (
+              <div key={item.item._id}>
+                <h3>name : {bill.find((e) => e._id === item.item._id)?.name}</h3>
+                <h5>price : {bill.find((e) => e._id === item.item._id)?.price}</h5>
+                <p>quantity: {item.quantity}</p>
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default Billing
+export default Billing;
